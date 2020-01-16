ad09f633c511eadf2a758663e1f7151a04bf0c02
marcello505 <marcellohaddeman@gmail.com>
2020-01-16 15:12:26 +0100

    Methodes getInstance() en getAllSeries() aangemaakt.
    Methodes aangemaakt. Column namen moeten waarschijnlijk nog moeten worden aangepast.

--- src/main/java/datalayer/SerieDAO.java
+++ src/main/java/datalayer/SerieDAO.java
@@ -1,16 +1,54 @@
 package datalayer;
 
 import datalayerinterface.ISerie;
+import models.Movie;
 import models.Profile;
 import models.Serie;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class SerieDAO implements ISerie {
+    private static SerieDAO instance;
+
+    public static SerieDAO getInstance(){
+        if(instance == null){
+            instance = new SerieDAO();
+        }
+        return instance;
+    }
 
     @Override
     public List getAllSeries() {
-        return null;
+        ArrayList<Serie> allSeries = new ArrayList<Serie>();
+        Connection conn = null;
+        try{
+            conn = MysqlDAO.getInstance().connect();
+            PreparedStatement getAllSeries = conn.prepareStatement("SELECT * FROM serie");
+            ResultSet resultSet = getAllSeries.executeQuery();
+
+            while(resultSet.next()) {
+                //Deze moeten nog aangepast worden voor de uiteindelijke column namen
+                int serieID = resultSet.getInt("serieID");
+                String serieTitle = resultSet.getString("serieTitle");
+                int serieAge = resultSet.getInt("serieAge");
+                String serieLanguage = resultSet.getString("serieLanguage");
+                String serieGenre = resultSet.getString("serieGenre");
+                String serieSuggestions = resultSet.getString("serieSuggestions");
+
+                Serie s = new Serie(serieID, serieTitle, serieAge, serieLanguage, serieGenre, serieSuggestions);
+                allSeries.add(s);
+            }
+        }catch(SQLException e){
+            e.printStackTrace();
+        }finally {
+            MysqlDAO.getInstance().closeConnection(conn);
+        }
+        return allSeries;
     }
 
     @Override
