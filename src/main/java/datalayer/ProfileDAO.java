6c7fded5dfc7c67fddb63e13b4c0cf092ed48a31
Coen Ribbens <cj.ribbens@student.avans.nl>
2020-01-16 15:22:17 +0100

    Started work on profileDAO

--- src/main/java/datalayer/ProfileDAO.java
+++ src/main/java/datalayer/ProfileDAO.java
@@ -4,13 +4,48 @@ import datalayerinterface.IProfile;
 import models.Account;
 import models.Profile;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProfileDAO implements IProfile {
+    private static ProfileDAO instance;
+
+    public ProfileDAO() {
+    }
+
+    public static ProfileDAO getInstance() {
+        if (instance == null) {
+            instance = new ProfileDAO();
+        }
+        return instance;
+    }
 
     @Override
     public List getAllProfiles() {
-        return null;
+        ArrayList<Account> allProfiles = new ArrayList<Account>();
+        Connection conn = null;
+
+        try{
+            conn = MysqlDAO.getInstance().connect();
+            PreparedStatement getAllProfiles = conn.prepareStatement("SELECT * FROM profile");
+            ResultSet resultSet = getAllProfiles.executeQuery();
+
+            while(resultSet.next()) {
+                int profileId = resultSet.getInt("profileId");
+                String profileName = resultSet.getString("profileName");
+                Date dateOfBirth = resultSet.getDate("dateOfBirth");
+                int accountId = resultSet.getInt("accountId");
+
+                Profile p = new Profile(profileId, profileName, dateOfBirth, accountId);
+                allProfiles.add(p);
+            }
+        }catch(SQLException e) {
+            e.printStackTrace();
+        }finally {
+            MysqlDAO.getInstance().closeConnection(conn);
+        }
+        return allProfiles;
     }
 
     @Override
