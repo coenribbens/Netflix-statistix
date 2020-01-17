870896e12537f9caeda627466bdc465a9499b3c6
marcello505 <marcellohaddeman@gmail.com>
2020-01-17 11:30:04 +0100

    accountScene layout af.

--- src/main/java/view/MainInterface.java
+++ src/main/java/view/MainInterface.java
@@ -1,9 +1,11 @@
 package view;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -31,17 +33,42 @@ public class MainInterface extends Application {
         ListView<String> accountWoonplaats = new ListView<String>();
             accountWoonplaats.getItems().add("Woonplaats");
 
+        //Objecten detailBox
+        TextArea detailFilmsBekeken = new TextArea("Bekeken films:\n\n");
+            detailFilmsBekeken.setEditable(false);
+        TextArea detailSeriesBekeken = new TextArea("Bekeken series:\n\n");
+        detailSeriesBekeken.setEditable(false);
+
         //Objecten leftPanel
         Button buttonAccount = new Button("Account");
+            buttonAccount.setDisable(true);
+        Button buttonProfiel = new Button("Profiel");
+        Button buttonFilm = new Button("Film");
+        Button buttonSerie = new Button("Serie");
+
+        //Objecten toolBar
+        Button buttonToevoegen = new Button("Toevoegen");
+        Button buttonBewerken = new Button("Bewerken");
+        Button buttonVerwijderen = new Button("Verwijderen");
 
 
 
-        BorderPane mainBP = new BorderPane();
         VBox leftPanel = new VBox();
-        mainBP.setLeft(leftPanel);
+            leftPanel.getChildren().addAll(buttonAccount, buttonProfiel, buttonFilm, buttonSerie);
         HBox listView = new HBox();
             listView.getChildren().addAll(accountNaam, accountStraat, accountHuisNummer, accountPostcode, accountWoonplaats);
-        mainBP.setCenter(listView);
+        HBox detailBox = new HBox();
+            detailBox.getChildren().addAll(detailFilmsBekeken, detailSeriesBekeken);
+        VBox centerView = new VBox();
+            centerView.getChildren().addAll(listView, detailBox);
+        HBox toolBar = new HBox();
+            toolBar.getChildren().addAll(buttonToevoegen, buttonBewerken, buttonVerwijderen);
+            toolBar.setAlignment(Pos.CENTER_RIGHT);
+
+        BorderPane mainBP = new BorderPane();
+            mainBP.setCenter(centerView);
+            mainBP.setLeft(leftPanel);
+            mainBP.setTop(toolBar);
 
 
         Scene scene = new Scene(mainBP);
