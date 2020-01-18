ef3536721072955c1ff924043e6e01f1c987090d
Marcello505 <marcellohaddeman@gmail.com>
2020-01-18 17:58:05 +0100

    Minimale grote van het venster aangegeven

--- src/main/java/view/MainInterface.java
+++ src/main/java/view/MainInterface.java
@@ -19,6 +19,8 @@ public class MainInterface extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         stage.setScene(mainScene(stage));
+        stage.setMinHeight(400);
+        stage.setMinWidth(700);
         stage.show();
     }
 
