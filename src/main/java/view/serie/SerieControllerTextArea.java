1a94ae76662ecd7eb19df267dc2f34128356bf2d
marcello505 <marcellohaddeman@gmail.com>
2020-04-12 23:04:27 +0200

    Serie suggestie toegevoegd.

--- src/main/java/view/serie/SerieControllerTextArea.java
+++ src/main/java/view/serie/SerieControllerTextArea.java
@@ -3,19 +3,23 @@ package view.serie;
 import datalayer.EpisodeDAO;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.input.MouseEvent;
 import models.Episode;
+import models.Serie;
 
 
 public class SerieControllerTextArea implements EventHandler<MouseEvent> {
 
     private TableView tableView;
     private TextArea textArea;
-    public SerieControllerTextArea(TableView tableView, TextArea textArea){
+    private ChoiceBox<Serie> serieChoiceBox;
+    public SerieControllerTextArea(TableView tableView, TextArea textArea, ChoiceBox<Serie> serieChoiceBox){
         this.tableView = tableView;
         this.textArea = textArea;
+        this.serieChoiceBox = serieChoiceBox;
     }
 
     /**
@@ -26,13 +30,15 @@ public class SerieControllerTextArea implements EventHandler<MouseEvent> {
     public void handle(MouseEvent actionEvent) {
         Episode episode = (Episode)this.tableView.getSelectionModel().getSelectedItem();
         EpisodeDAO episodeDAO = EpisodeDAO.getInstance();
+        String stringTextArea = "Deze serie lijkt heel erg op de serie: " + this.serieChoiceBox.getSelectionModel().getSelectedItem().getSuggestions();
 
 
         // Onderstaande formule vereist doubles voor comma getallen, integers werken niet. 0.9 is voor een int nog altijd een 0.
        double avgwatchtime = (double)episodeDAO.getAverageWatchTimeForEpisode(episode) / Double.parseDouble(episode.getDuration()) * 100;
 
        int y = (int) avgwatchtime; // casten de double terug naar een int, omdat we niet 10 decimalen in de output willen.
-        this.textArea.setText("Deze episode is gemiddeld " + y + "% bekeken.");
+        stringTextArea += "\nDeze episode is gemiddeld " + y + "% bekeken.";
+        this.textArea.setText(stringTextArea);
 
     }
 }
